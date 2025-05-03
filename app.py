617f12f42af4be39ff64c27a4f3b1c4a52b929cc
AmritMrar <amritmrar047@gmail.com>
2025-05-03 19:02:24 +0530

    Debug disabled

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def favicon():
     return '', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=False)
