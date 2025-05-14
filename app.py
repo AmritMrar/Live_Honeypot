e18c98237cc35264d48108164884d7f30cb3545e
AmritMrar <amritmrar047@gmail.com>
2025-05-14 15:26:31 +0530

    port 5050

--- app.py
+++ app.py
@@ -119,4 +119,4 @@ def favicon():
     return '', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=False)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5050)), debug=False)
